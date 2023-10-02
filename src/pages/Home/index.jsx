7799d313830f5c036b5a340c9b2260c9cdfae708
prudrakshula <prudrakshula@nouveau-labs.com>
2023-10-02 16:22:37 +0530

    added content to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./index.css"
 
 function Home() {
   return (
-    <div>Home Page</div>
+    <div className="home-wrapper">
+      <h1>Welcome to Weather App</h1>
+      <p>We can find weather reports by <i>city</i> name</p>
+      <p>
+        Let's go to <NavLink to="/weather">Weather reports</NavLink>
+      </p>
+    </div>
   )
 }
 
